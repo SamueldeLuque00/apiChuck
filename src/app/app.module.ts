4aa4f71f86923e9f3588859fce369aeaf8cf1f96
Samuel <s.rodriguez2003q@gmail.com>
2022-10-26 16:13:29 -0500

    Api de chuck terminada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ChuckModule } from "./chuck/chuck.module";
+import { HttpClientModule } from "@angular/common/http";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule, 
+    ChuckModule
   ],
   providers: [],
   bootstrap: [AppComponent]
